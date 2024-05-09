f6f5297c339d4767734352e10cb96b459fbca93b
Rhijn Wiegman <rhijn.wiegman@student.uva.nl>
2024-05-09 16:21:17 +0200

    generate baby demo using STB-VMM

--- STB-VMM-master/models/.ipynb_checkpoints/model-checkpoint.py
+++ STB-VMM-master/models/.ipynb_checkpoints/model-checkpoint.py
@@ -147,9 +147,9 @@ def window_partition(x, window_size):
     '''
     B, H, W, C = x.shape
     #   Debug
-    #print("B, H, W, C")
-    #print(window_size)
-    #print(x.shape)
+    # print("B, H, W, C")
+    # print(window_size)
+    # print(x.shape)
     x = x.view(B, H // window_size, window_size, W // window_size, window_size, C)
     windows = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(-1, window_size, window_size, C)
     return windows
